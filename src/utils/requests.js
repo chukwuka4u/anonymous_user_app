3650cc5042ebb5011891d51cc762a3767c00a646
Chukwukadibia <chukwukadiemma@gmail.com>
2026-02-12 10:05:07 +0100

    updated api url and completed ui

--- src/utils/requests.js
+++ src/utils/requests.js
@@ -1,12 +1,12 @@
 async function getQueue(adminId, queueId) {
-    const res = await fetch(`http://localhost:3000/api/v1/user/anonymous/${queueId}/get_queue?admin_id=${adminId}`);
+    const res = await fetch(`https://queue-api-48vb.onrender.com/api/v1/user/anonymous/${queueId}/get_queue?admin_id=${adminId}`);
     console.log(adminId, queueId)
     const queue = await res.json();
-    console.log(queue)
+    return queue
 }
 
 async function joinQueue(adminId, queueId) {
-    const res = await fetch(`http://localhost:3000/api/v1/user/anonymous/${queueId}/anonymous_join?admin_id=${adminId}`, {
+    const res = await fetch(`https://queue-api-48vb.onrender.com/api/v1/user/anonymous/${queueId}/anonymous_join?admin_id=${adminId}`, {
         method: 'POST'
     });
     const data = await res.json()
@@ -14,8 +14,8 @@ async function joinQueue(adminId, queueId) {
 }
 
 async function leaveQueue(adminId, queueId, userId) {
-    const res = await fetch(`http://localhost:3000/api/v1/user/anonymous/${queueId}/anonymous_leave?admin_id=${adminId}&user_id=${userId}`, {
-        method: 'DELETE'
+    const res = await fetch(`https://queue-api-48vb.onrender.com/api/v1/user/anonymous/${queueId}/anonymous_leave?admin_id=${adminId}&user_id=${userId}`, {
+        method: 'PATCH'
     });
     const data = await res.json()
     return data;
